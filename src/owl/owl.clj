d5504e2056381611a97d5a9656d0162787aaabc7
Phillip Lord <phillip.lord@newcastle.ac.uk>
2012-10-17 12:31:25 +0100

    Prefix support in place.

--- src/owl/owl.clj
+++ src/owl/owl.clj
@@ -1,38 +1,50 @@
-
 (ns owl.owl
   (:require [owl.util :as util])
+  (:refer-clojure :exclude [some only comment])
   (:import
    (org.semanticweb.owlapi.model OWLOntologyManager OWLOntology IRI
                                  OWLClassExpression OWLClass OWLAnnotation)
    (org.semanticweb.owlapi.apibinding OWLManager)
    (org.coode.owlapi.manchesterowlsyntax ManchesterOWLSyntaxOntologyFormat)
    (org.semanticweb.owlapi.io StreamDocumentTarget)
-   (java.io ByteArrayOutputStream)
+   (org.semanticweb.owlapi.util DefaultPrefixManager)
+   (java.io ByteArrayOutputStream FileOutputStream PrintWriter)
    (java.io File)
    (org.semanticweb.owlapi.model AddAxiom)))
 
-(def ontology-data-factory (OWLManager/getOWLDataFactory))
+(def
+  ^{:doc "A java object which is the main factory for all other objects"
+    :private true}
+  ontology-data-factory
+  (OWLManager/getOWLDataFactory))
 
-(def ^{:dynamic true} current-ontology nil)
+(def ^{:dynamic true
+       :doc
+       "The current ontology is either the last one defined using `defontology'
+or set using `set-current-ontology'"
+}
+  current-ontology nil)
 
-(defrecord Ontology [iri file manager ontology])
-(defmacro defontology [name & body]
+(defrecord Ontology [iri file prefix manager ontology])
+(defmacro defontology
+  "Define a new ontology"
+  [name & body]
   `(do
      (let [options# (apply hash-map '~body)
            iri# (IRI/create (:iri options#))
            manager# (OWLManager/createOWLOntologyManager)]
-       (println iri#)
-       (println manager#)
        (def ~name
          (Ontology.
           iri#
           (:file options#)
+          (:prefix options#)
           manager#
-          (.createOntology manager# iri#))))
-     (owl.owl/set-current-ontology ~name)))
-
-
-(defn set-current-ontology[ontology]
+          (.createOntology manager# iri#)))
+     (owl.owl/set-current-ontology ~name))))
+  
+(defn set-current-ontology
+  "Sets the current ontology as defined by `defontology'"
+  [ontology]
   ;;check type
   (def ^{:dynamic true} current-ontology ontology))
 
@@ -62,24 +74,41 @@
       (throw (IllegalStateException. "No current ontology manager")))
     manager))
 
+(defn get-current-prefix []
+  (let [prefix (:prefix (get-current-ontology))]
+    (when (nil? prefix)
+      (throw (IllegalStateException. "No current prefix")))
+    prefix))
 
 (defn save-ontology
   ([]
      (save-ontology (:file (get-current-ontology))))
   ([filename]
      (let [file (new File filename)
-           document-iri (IRI/create file)]
+           output-stream (new FileOutputStream file)
+           file-writer (new PrintWriter output-stream)
+           manchester-format (ManchesterOWLSyntaxOntologyFormat.)
+           format (.getOntologyFormat (get-current-manager)
+                                      (get-current-jontology))
+           ]
+       (.println file-writer "## This file was created by Clojure-OWL" )
+       (.println file-writer "## It should not be edited by hand" )
+       (.flush file-writer)
+       (.setPrefix manchester-format (get-current-prefix)
+                   (str (.toString (get-current-iri)) "#"))
        (.saveOntology (get-current-manager) (get-current-jontology)
-                      (new ManchesterOWLSyntaxOntologyFormat) document-iri))))
+                      manchester-format output-stream))))
+
+(defn iriforname [name]
+  (IRI/create (str (get-current-iri) "#" name)))
 
 (defn get-create-object-property [name]
   (.getOWLObjectProperty ontology-data-factory
-                         (IRI/create (str (get-current-iri) "#" name))))
-
+                         (iriforname name)))
 
 (defn get-create-class [name]
   (.getOWLClass ontology-data-factory
-                (IRI/create (str (get-current-iri) "#" name))))
+                (iriforname name)))
 
 (defn ensure-class [clz]
   (cond (instance? org.semanticweb.owlapi.model.OWLClassExpression clz)
@@ -93,7 +122,6 @@
                 (new AddAxiom (get-current-jontology)
                      (frame-adder (ensure-class name) frame))))
   
-
 (defn add-frame
   [frame-adder name frame]
   (dorun
@@ -130,7 +158,6 @@
 (defn add-class[name]
   (add-one-frame create-class-axiom name ""))
 
-
 ;; object properties
 (defn objectproperty
   [name]
@@ -139,9 +166,6 @@
                      (.getOWLDeclarationAxiom
                       ontology-data-factory
                       (get-create-object-property name)))))
-
-
-
 
 ;; restrictions!
 (defn some [property class]
@@ -179,7 +203,6 @@
       annotation-property 
       (.getOWLLiteral ontology-data-factory literal language))))
 
-
 ;; various annotation types
 (def label
   (partial annotation (.getRDFSLabel ontology-data-factory)))
@@ -193,17 +216,14 @@
 (def seealso
   (partial annotation (.getRDFSSeeAlso ontology-data-factory)))
 
-
 (defn owlclass-explicit
   ([name frames]
      (add-class name)
      (add-subclass name (:subclass frames))
      (add-equivalent name (:equivalent frames))
-     (println (:annotation frames))
      (add-annotation name (:annotation frames)))
   ([name]
      (owlclass-explicit name {})))
-
 
 (defn owlclass
   ([name & frames]
