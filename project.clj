e5da0c247861ae095344a86d9cf396c50a673feb
Phillip Lord <phillip.lord@newcastle.ac.uk>
2012-10-30 11:56:13 +0000

    Added elk as dependency.

--- project.clj
+++ project.clj
@@ -2,9 +2,14 @@
   :description "Help to define a project in OWL"
   :plugins [[lein-swank "1.4.4"]]
   :repositories [["maven" "http://repo1.maven.org/maven2"]]
-  :dependencies [[net.sourceforge.owlapi/owlapi-api "3.4"]
-                 [net.sourceforge.owlapi/owlapi-apibinding "3.4"]
-                 [org.clojure/clojure "1.4.0"]])
+  :dependencies [[net.sourceforge.owlapi/owlapi-api "3.4.1"]
+                 [net.sourceforge.owlapi/owlapi-apibinding "3.4.1"]
+                 [net.sourceforge.owlapi/owlapi-distribution "3.4.1"]
+
+                 [org.clojure/clojure "1.4.0"]
+                 
+                 ;; reasoners
+                 [org.semanticweb.elk/elk-owlapi "0.3.1"]])
 
 
 
