79712b66bef144f735a7ae47075730aead1d1098
Phillip Lord <phillip.lord@newcastle.ac.uk>
2012-11-05 09:09:04 +0000

    Groupify now uses recur rather than recursing!

--- src/owl/util.clj
+++ src/owl/util.clj
@@ -8,10 +8,11 @@
 (defn groupify
   "Takes a list with keywords and values and turns all adjacent values into a single list"
   ;; entry point
-  ([list]
-     (groupify () list nil nil))
-  ;; not an entry point!
-  ([reduced-frame long-frame-left current-keyword current-val-list]
+  [list]
+  (loop [reduced-frame ()
+         long-frame-left list
+         current-keyword nil
+         current-val-list nil]
      (if-let [first-val (first long-frame-left)]
        ;; the unreduced frame is not yet empty
        (if (keyword? first-val)
@@ -21,17 +22,17 @@
            (if (not current-val-list)
              (throw (IllegalArgumentException. "Cannot have two adjacent keywords"))
              ;; and we have some existing keywords and values
-             (groupify
+             (recur
               (cons current-keyword
                     (cons current-val-list
                           reduced-frame))
               (rest long-frame-left) first-val nil))
            ;; so we have a legal new keyword, so start a new list
-           (groupify
+           (recur
             reduced-frame (rest long-frame-left)
             first-val nil))
          ;; we do not have a keyword
-         (groupify reduced-frame (rest long-frame-left)
+         (recur reduced-frame (rest long-frame-left)
                    current-keyword (cons first-val current-val-list)))
        
        ;; else we have nothing left in the frame, so terminate
