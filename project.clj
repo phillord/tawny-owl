4ff9bbd936b0c84a17f81af68db6dcbdf59de28a
Phillip Lord <phillip.lord@newcastle.ac.uk>
2012-11-05 09:05:38 +0000

    Added HermiT from Berkeley repo

--- project.clj
+++ project.clj
@@ -1,17 +1,29 @@
-(defproject owl "1.0.0-SNAPSHOT"
+(defproject clojure-owl "1.0.0-SNAPSHOT"
   :description "Help to define a project in OWL"
+  :url "http://code.google.com/p/clojure-owl/"
   :plugins [[lein-swank "1.4.4"]]
-  :repositories [["maven" "http://repo1.maven.org/maven2"]]
-  :dependencies [[net.sourceforge.owlapi/owlapi-api "3.4.1"]
+  :repositories [["maven" "http://repo1.maven.org/maven2"]
+                 ;; getting hermit from here
+                 ["berkley" "http://code.berkeleybop.org/maven/repository/"]
+                 ]
+  :dependencies [
+                 ;; owl API
+                 [net.sourceforge.owlapi/owlapi-api "3.4.1"]
                  [net.sourceforge.owlapi/owlapi-apibinding "3.4.1"]
                  [net.sourceforge.owlapi/owlapi-distribution "3.4.1"]
 
+                 ;; clojure
                  [org.clojure/clojure "1.4.0"]
                  
                  ;; reasoners
-                 [org.semanticweb.elk/elk-owlapi "0.3.1"]])
+                 [org.semanticweb.elk/elk-owlapi "0.3.1"]
+                 [org.semanticweb/HermiT "1.3.6-BBOP"]
+                 
+
+                 ]
 
 
 
 
 
+)
