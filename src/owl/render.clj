828188c1355eb866f7376161eaf2a8585270812e
Phillip Lord <phillip.lord@newcastle.ac.uk>
2012-11-19 10:20:01 +0000

    Rendering to Clojure forms.

--- src/owl/render.clj
+++ src/owl/render.clj
@@ -44,15 +44,108 @@
                                          )))
 
 
+(declare form)
+
+(defmulti as-form class)
+
+(defmethod as-form AxiomedEntity [e]
+  (as-form (:entity e)))
+
+(defmethod as-form OWLClass [c]
+  (let [super (.getSuperClasses c (owl/get-current-jontology))
+        equiv (.getEquivalentClasses c (owl/get-current-jontology))
+        disjoint (.getDisjointClasses c (owl/get-current-jontology))]
+    `(owl.owl/defclass ~(form c)
+       ~@(when (< 0 (count super))
+           (cons
+            :subclass
+            (form super)))
+       ~@(when (< 0 (count equiv))
+           (cons
+            :equivalent
+            (form equiv)))
+       ~@(when (< 0 (count disjoint))
+           (cons
+            :disjoint
+            (form disjoint))))))
+
+       
+(defmulti form class)
+
+(defmethod form Set [s]
+  (map #(form %) s))
+
+(defmethod form OWLClass [c]
+  (.getFragment (.getIRI c)))
+
+(defmethod form OWLObjectProperty [p]
+  (.getFragment (.getIRI p)))
+
+(defmethod form OWLObjectSomeValuesFrom [s]
+  (list 'owlsome
+        (form (.getProperty s))
+        (form (.getFiller s))))
+
+(defmethod form OWLObjectUnionOf [u]
+  (list 'owlor (form (.getOperands u))))
+
+(defmethod form OWLObjectIntersectionOf [c]
+  (list 'owland (form (.getOperands c))))
+
+(defmethod form OWLObjectAllValuesFrom [a]
+  (list 'owlall
+        (form (.getProperty a))
+        (form (.getFiller a))))
+
+(defmethod form OWLObjectComplementOf [c]
+  (list 'owlnot
+        (form (.getOperand c))))
+
+(defmethod form OWLObjectExactCardinality [c]
+  (list 'exactly (.getCardinality c)
+        (form (.getProperty c))
+        (form (.getFiller c))))
+
+
+(defmethod form OWLObjectMaxCardinality [c]
+  (list 'atmost (.getCardinality c)
+        (form (.getProperty c))
+        (form (.getFiller c))))
+
+(defmethod form OWLObjectMinCardinality [c]
+  (list 'atleast (.getCardinalty c)
+        (form (.getProperty c))
+        (form (.getFiller c))))
+
+
+;; OWLObjectHasSelf
+;; OWLObjectHasValue
+;; OWLObjectIntersectionOf
+;; OWLObjectOneOf
+;; OWLObjectUnionOf
+
+
+
+
+
+(defmethod form :default [e]
+  `(not-sure-what-to-do))
+
+
 (defmulti as-text class)
 
 (defmethod as-text AxiomedEntity [e]
   (as-text (:entity e)))
 
+(declare text)
 (defmethod as-text OWLClass [c]
   (str (text c)
-       "\n\t:subclass"
-       (text (.getSuperClasses c (owl/get-current-jontology)))))
+       "\n\t:subclass\n"
+       (text (.getSuperClasses c (owl/get-current-jontology)))
+       "\n\t:equivalent\n"
+       (text (.getEquivalentClasses c (owl/get-current-jontology)))
+       "\n\t:disjoint\n"
+       (text (.getDisjointClasses c (owl/get-current-jontology)))))
 
 (defmulti text class)
 
