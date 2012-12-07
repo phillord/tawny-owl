6f9af1bed282d5f2faee6ee727977482aa397ada
Phillip Lord <phillip.lord@newcastle.ac.uk>
2012-12-07 08:14:10 +0000

    Updated for 0.3

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject uk.org.russet/clojure-owl "0.3-SNAPSHOT"
+(defproject uk.org.russet/clojure-owl "0.3"
   :description "Clojure-owl enables building OWL ontologies in a programmatic environment."
   :url "http://www.russet.org.uk/blog/clojure-owl"
   :plugins [[lein-swank "1.4.4"]]
   :repositories [["maven" "http://repo1.maven.org/maven2"]
 
+                 ;; hermit comes from here
                  ["phillord" "http://homepages.cs.ncl.ac.uk/phillip.lord/maven"]
-                 ;; getting hermit from here
-                 ;; ["berkley" "http://code.berkeleybop.org/maven/repository/"]
                  ]
 
   :resource-paths ["src/resources"]
@@ -20,16 +19,16 @@
 
   :dependencies [
                  ;; owl API
-                 [net.sourceforge.owlapi/owlapi-api "3.4.1"]
-                 [net.sourceforge.owlapi/owlapi-apibinding "3.4.1"]
-                 [net.sourceforge.owlapi/owlapi-distribution "3.4.1"]
+                 [net.sourceforge.owlapi/owlapi-api "3.4.2"]
+                 [net.sourceforge.owlapi/owlapi-apibinding "3.4.2"]
+                 [net.sourceforge.owlapi/owlapi-distribution "3.4.2"]
 
                  ;; clojure
                  [org.clojure/clojure "1.4.0"]
                  
                  ;; reasoners
                  [org.semanticweb.elk/elk-owlapi "0.3.1"]
-                 [org.semanticweb.hermit/HermiT "1.3.7-SNAPSHOT"]
+                 [org.semanticweb.hermit/HermiT "1.3.6.1"]
                  
 
                  ]
