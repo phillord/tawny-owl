dd492dbf2a3630f823a555c5aa391027fcb6bf0f
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-01-11 14:54:47 +0000

    Major rework of memorise
    Duplication from lookup.clj has gone.
    Mappings changed to IRI->set (rather than list).
    Merging cleaned, using set API.
    Sorted serialized form, ordered by IRI

--- src/tawny/memorise.clj
+++ src/tawny/memorise.clj
@@ -15,90 +15,46 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see http://www.gnu.org/licenses/.
 
-
 (ns tawny.memorise
-  (:require [tawny.owl]))
+  (:require [tawny.owl]
+            [tawny.lookup]
+            [clojure.set]))
 
 
-;; this code is almost identical to lookup
-(defn- iri-to-var [var]
-  
-  (if (tawny.owl/named-object? (var-get var))
-    (.toString (.getIRI (tawny.owl/as-named-object (var-get var))))
-    nil))
+(defn- var-str 
+  "Given a var, return a string representation of its name"
+  [var]
+  (str (:name (meta var))))
 
-(defn- pairs-iri-to-var [namespace]
-  (for  [k (vals  (ns-publics namespace))]
-    [(iri-to-var k) k]))
 
-(defn- filtered-iri-to-vars [namespace]
-  (filter
-   (comp not nil? first)
-   (pairs-iri-to-var namespace)))
+(defn- change-values-to-string-set [map]
+  (into {}
+        (for [[k v] map]
+          [k #{(var-str v)}])))
 
-(defn map-iri-to-var [namespace]
-  (apply hash-map (flatten (filtered-iri-to-vars namespace))))
 
+(defn- find-missing-mappings 
+  "Find mappings in old that are not present in current.
+
+Both current and old are maps of IRI to set of string names"
+  [current old]
+  (into {}
+        (filter (comp not empty? second)
+                (merge-with 
+                 clojure.set/difference old current))))
+
+(defn merge-with-distinct [x y]
+  (into (sorted-map) (merge-with clojure.set/union x y)))
 
 
 (defn memorise-map
   "Returns a map of entities to remember.
 
-This function returns a map from the IRI to the mapping that is currently
-being used."
+This function returns a map from the IRI to the var object(s) which hold it"
   ([]
      (memorise-map *ns*))
   ([namespace]
-     (into {} (for [[k v] (map-iri-to-var namespace)] [k v]))))
-
-
-(declare fetch-old-and-current-entities)
-(defn memorise
-   "Save current memorise information in file."
-   [file]
-   (with-open [w (clojure.java.io/writer file)]
-     (.write w "(tawny.memorise/memory\n")
-     (doseq [[k v] (fetch-old-and-current-entities)]
-       (doseq [name v]
-         (println name)
-         (println k)
-         ;;(println name " " k)
-         (.write w (format "\"%s\" \"%s\"\n" k name ))))
-     (.write w "\n)")))
-     
-
-
-(defn fetch-current-entities []
-  (into {}
-        (for [[k v] (memorise-map)]
-          [k [(str (:name (meta v)))]])))
-
-(defn fetch-remembered-entities []
-  (:remember (deref (:options (tawny.owl/get-current-ontology)))))
-
-
-(defn fetch-old-and-current-entities []
-  ;; sort so the print order is defined
-  (sorted-map 
-   (merge-with (comp distinct concat)
-               (fetch-current-entities)
-               (fetch-remembered-entities))))
-
-
-(declare check-old-mappings)
-(defn remember
-  "Restore the current memorise information from file"
-  [file]
-   (let [iri-to-name-mapping
-         ;; think this is going to be very dependant on the current directory.
-         ;; So it's not going to work if another project imports the OBI
-         ;; namespace.
-         (load-file file)]
-     (dosync
-      (alter
-       (:options (tawny.owl/get-current-ontology))
-       merge {:remember iri-to-name-mapping}))
-     (check-old-mappings iri-to-name-mapping (memorise-map))))
+     (into {} (for [[k v] (tawny.lookup/iri-to-var namespace)] [k v]))))
 
 
 (defn generate-obsolete-mapping
@@ -108,7 +64,7 @@ being used."
     (fn [x]
       (println "Generating mapping to obsolete symbol:" x " to " mapping)
       (intern *ns*
-              (symbol  x) 
+              (symbol x) 
               (fn []
                 (println x " has changed its symbol please use " 
                          mapping " instead")
@@ -116,36 +72,87 @@ being used."
     old-mappings)))
 
 
+
+(defn fetch-remembered-entities []
+  (or  
+   (:remember (deref (:options (tawny.owl/get-current-ontology))))
+   {}))
+
+
+(defn fetch-old-and-current-entities []
+  (merge-with-distinct
+    (change-values-to-string-set (memorise-map))
+    (fetch-remembered-entities)))
+
 (defn- check-old-mappings 
   "Check old mappings that we have remembered against those that we would
 memorise now. Check that old mappings are still in memorise, otherwise create
-a new mapping."
+a new mapping.
+
+remember is a map iri to a set of names
+memorise is a map iri to current var
+
+"
   [remember memorise]
-  (doseq [[iri mapping] memorise]
-    (let [remember-mappings (get remember iri)]
-      (when (seq remember-mappings)
-        (let [old-mappings
-              (doall 
-               (remove
-                #(= (str (:name (meta mapping))) %) remember-mappings))]
-          (when (seq old-mappings)
-            (generate-obsolete-mapping old-mappings mapping)))))))
+
+  (let 
+      ;; memorise is iri to var, so need to get this as a string
+      [memorise-iri-to-str
+       (change-values-to-string-set memorise)
+       ;; find the mappings that we are missing
+       missing-mappings
+       (find-missing-mappings memorise-iri-to-str remember)
+       ]
+
+    ;; generate new symbols for everything
+    (doseq [[iri old-symbol-string] missing-mappings]
+
+      (generate-obsolete-mapping
+       old-symbol-string (get memorise iri)))))
+
 
 
 (defn- memory-merge
-  ([list]
-     (memory-merge list {}))
-  ([remaining hash]
-     (if (seq remaining)
-       (recur 
-        (rest (rest remaining))
-        (merge-with concat
-                    (hash-map
-                     (first remaining)
-                     (list (second remaining)))
-                    hash))
-       hash)))
+"Accepts key and values as pairs, but preserves duplicate values for the
+same keys, as all values are represented as sets"
+  [items]
+  (reduce 
+   (partial merge-with 
+            clojure.set/union)
+   (map #(hash-map (first %) (hash-set (second %)))
+        (partition 2 items))))
 
-(defn memory [& list]
+
+(defn memory 
+  "Returns a map of IRI to set of string names for that concept"
+  [& list]
   (memory-merge list))
+
+(defn memorise
+   "Save current memorise information in file."
+   [file]
+   (with-open [w (clojure.java.io/writer file)]
+     (.write w "(tawny.memorise/memory\n")
+     (doseq [[k v] (fetch-old-and-current-entities)]
+       (doseq [name v]
+         (.write w (format "\"%s\" \"%s\"\n" k name))))
+     (.write w "\n)")))
+     
+(defn remember
+  "Restore the current memorise information from file"
+  [file]
+   (let [iri-to-name-mapping
+         ;; think this is going to be very dependant on the current directory.
+         ;; So it's not going to work if another project imports the OBI
+         ;; namespace.
+         
+         ;; the file contains a single "memory" form.
+         (load-file file)]
+     ;; store everything that we load, because we will need to save out
+     ;; everything we load.
+     (dosync
+      (alter
+       (:options (tawny.owl/get-current-ontology))
+       merge {:remember iri-to-name-mapping}))
+     (check-old-mappings iri-to-name-mapping (memorise-map))))
 
---
+++ test/tawny/memorise_test.clj
@@ -0,0 +1,154 @@
+
+(ns tawny.memorise-test
+  (:use [clojure.test])
+  (:require [tawny.memorise :as m]
+            [tawny.owl :as o]))
+
+
+(defn test-memorise-map []
+  (m/memorise-map *ns*))
+
+
+(defn bind-some-vars []
+  (eval 
+   '(do 
+      (tawny.owl/defclass a)
+      (tawny.owl/defclass b)
+      (tawny.owl/defclass c))))
+
+(defn- unbind-some-vars []
+  (ns-unmap *ns* 'a)
+  (ns-unmap *ns* 'b)
+  (ns-unmap *ns* 'c))
+
+(deftest bind-and-unbind
+  (is 
+   (= 0 (count 
+         (do (o/test-ontology)
+             (test-memorise-map)))))
+  
+  (is 
+   (= 3 
+      (do (o/test-ontology)
+          (bind-some-vars)
+          (let [x
+                (count
+                 (test-memorise-map))]
+            (unbind-some-vars)
+            x))))
+
+  (is 
+   (= 0 (count 
+         (do (o/test-ontology)
+             (bind-some-vars)
+             (unbind-some-vars)
+             (test-memorise-map)))))
+
+  (is
+   (= 0
+      (do 
+        (o/test-ontology)
+        (count (test-memorise-map))))))
+
+
+
+(deftest memory-merge []
+  (is 
+   (= {:a #{:b} :c #{:d} :e #{:f}}
+      (#'m/memory-merge '(:a :b :c :d :e :f))
+      ))
+
+  (is 
+   (= {:a #{:e :b}, :c #{:f :d} :g #{:h}}
+      (#'m/memory-merge '(:a :b, :c :d, :a :e, :c :f, :g :h))
+      )))
+
+
+(deftest find-missing-mappings
+  (is
+   (= {}
+      (#'m/find-missing-mappings
+       {"iri1" #{"symbol1"}}
+       {"iri1" #{"symbol1"}})))
+
+  (is 
+   (= {}
+      (#'m/find-missing-mappings
+       {"iri1" #{"symbol1" "symbol2"}}
+       {"iri1" #{"symbol1"}})))
+  
+  (is
+   (= {"iri1" #{"symbol2"}} 
+      (#'m/find-missing-mappings
+       {"iri1" #{"symbol1"}}
+       {"iri1" #{"symbol1" "symbol2"}})))
+  
+  )
+
+
+(def var-str-test 1)
+(deftest var-str
+  (is 
+   (= "var-str-test"
+      (#'m/var-str (var var-str-test)))))
+
+
+(deftest memory
+  (is 
+   (= {"a" #{"1"}}
+      (m/memory "a" "1")
+      ))
+  (is
+   (= {"a" #{"1" "2"}}
+      (m/memory "a" "1" "a" "2")
+      )))
+
+
+(deftest change-values-to-string-set
+  (is
+   (= {"http://iri/#a" #{"a"}, 
+       "http://iri/#c" #{"c"}, 
+       "http://iri/#b" #{"b"}}
+      (do (o/test-ontology)
+          (bind-some-vars)
+          (let [retn 
+                (#'m/change-values-to-string-set (m/memorise-map *ns*))]
+            (unbind-some-vars)
+            retn))))
+
+  (is 
+   (= {}
+      (#'m/change-values-to-string-set (m/memorise-map *ns*)))))
+
+
+
+(deftest merge-with-distinct
+  (is 
+   (= {"a" #{"1"}}
+      (m/merge-with-distinct 
+        {} {"a" #{"1"}})))
+  
+  (is 
+   (= {"a" #{"1"}}
+      (m/merge-with-distinct 
+        {"a" #{"1"}} {})))
+
+
+  (is 
+   (= {"a" #{"1"}}
+      (m/merge-with-distinct
+        {"a" #{"1"}} {"a" #{"1"}})))
+
+
+(is 
+   (= {"a" #{"1" "2"}}
+      (m/merge-with-distinct
+        {"a" #{"1"}} {"a" #{"1" "2"}}))))
+
+(m/fetch-remembered-entities)
+(m/memorise-map)
+
+(m/fetch-old-and-current-entities)
+
+(o/test-ontology)
+(bind-some-vars)
