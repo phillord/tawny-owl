572bfaadcf83b929a8d65d7241461fafd7a14cfe
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-01-14 12:26:08 +0000

    Initial Checkin of polyglot.
    Have a function to generate empty properties file.

---
+++ src/tawny/polyglot.clj
@@ -0,0 +1,45 @@
+;; The contents of this file are subject to the LGPL License, Version 3.0.
+
+;; Copyright (C) 2013, Phillip Lord, Newcastle University
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses/.
+
+
+(ns tawny.polyglot
+  ;;(:use [i18n.core])
+  (:require [tawny.lookup]))
+
+
+;; oh dear, lifted from memorise -- move to lookup I think
+(defn- var-str 
+  "Given a var, return a string representation of its name"
+  [var]
+  (str (:name (meta var))))
+
+
+
+
+;; function to create empty properties file
+
+(defn polyglot-create-resource [filename] 
+  (with-open [w (clojure.java.io/writer filename)]
+    
+    (doseq [name 
+            (into (sorted-set)
+                  (for [[k v] (tawny.lookup/iri-to-var *ns*)]
+                    (var-str v)))]
+      (.write w (format "%s=\n" name)))))
+
+;; main entry function to gather add labels to all classes, given a language,
+;; warning of missing translations.
