c66d53e82a79ea0486b4199ac59b096640117c4a
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-03-06 16:22:26 +0000

    M-x delete-trailing-whitespace.

--- test/tawny/read_test.clj
+++ test/tawny/read_test.clj
@@ -26,31 +26,31 @@
   )
 
 (defn get-go-ontology []
-  (tawny.owl/remove-ontology-maybe 
+  (tawny.owl/remove-ontology-maybe
    (OWLOntologyID. (IRI/create "http://purl.obolibrary.org/obo/go.owl")))
   (.loadOntologyFromOntologyDocument
-   tawny.owl/owl-ontology-manager 
+   tawny.owl/owl-ontology-manager
    (IRI/create (clojure.java.io/resource "go-snippet.owl"))))
 
 
 (deftest read
-  (is 
-   (do 
+  (is
+   (do
      (try
-       ;; at some point this needs support in tawny. 
+       ;; at some point this needs support in tawny.
        (.addIRIMapper
         tawny.owl/owl-ontology-manager
         (SimpleIRIMapper.
          (IRI/create "http://www.w3.org/TR/2003/PR-owl-guide-20031209/food")
          (IRI/create (clojure.java.io/resource "food.rdf"))))
-       
-       (r/read :location 
+
+       (r/read :location
                (IRI/create (clojure.java.io/resource "wine.rdf"))
                :iri "http://www.w3.org/TR/2003/CR-owl-guide-20030818/wine"
                :prefix "wine:"
                )
-       ;; we shouldn't be using this again, but clean up anyway. 
-       (finally 
+       ;; we shouldn't be using this again, but clean up anyway.
+       (finally
          (.clearIRIMappers tawny.owl/owl-ontology-manager))))))
 
 
@@ -61,13 +61,13 @@
   (is
    (every?
     (comp not nil?)
-    (filter #'r/default-filter 
+    (filter #'r/default-filter
             (.getSignature (get-go-ontology)))))
 
   (is
    (every?
     (comp not nil?)
-    (filter (partial r/iri-starts-with-filter 
+    (filter (partial r/iri-starts-with-filter
                      "http://purl.obolibrary.org/obo/go")
             (.getSignature (get-go-ontology))))))
 
@@ -81,8 +81,8 @@
     (comp not nil?)
     (let [fil
           (doall
-           (map #'r/default-transform 
-                ;; this transform works on the anntotations which start with 
+           (map #'r/default-transform
+                ;; this transform works on the anntotations which start with
                 ;; "obo/go" rather than "obo/GO"
                 (filter (partial
                          r/iri-starts-with-filter "http://purl.obolibrary.org/obo/go")
@@ -90,50 +90,45 @@
       ;; let form allows me to add println -- not totally stupid
       ;;(println "first fil:" fil)
       fil)))
-  
-  
+
+
   (is
    (do
-     (let [fil 
+     (let [fil
            (doall
             (map r/label-transform
                  ;; the label transform should work on "GO" terms should return
                  ;; one annotation and one nil which it is superclass. We have
                  ;; chopped the annotation out of the snippet.
-                 (filter (partial r/iri-starts-with-filter 
+                 (filter (partial r/iri-starts-with-filter
                                   "http://purl.obolibrary.org/obo/GO")
                          (.getSignature (get-go-ontology)))))]
        ;;(println "This Fil:" fil)
        fil
        ))
    )
-  
+
   (is
    (thrown?
     IllegalArgumentException
-    (let [fil 
+    (let [fil
           (doall
            (map r/exception-nil-label-transform
-                      ;; this should throw an exception because we have one class without 
+                      ;; this should throw an exception because we have one class without
                       ;; annotation
-                      (filter (partial r/iri-starts-with-filter 
+                      (filter (partial r/iri-starts-with-filter
                                        "http://purl.obolibrary.org/obo/GO")
                               (.getSignature (get-go-ontology)))))]
       ;;(println  "Fil 1:" fil)
       fil
      )
     ))
-  
+
 
   )
 
 (deftest go-snippet-read
-  (is (r/read :location 
+  (is (r/read :location
               (IRI/create (clojure.java.io/resource "go-snippet.owl"))
               :iri "http://purl.obolibrary.org/obo/go.owl"
               :prefix "go:")))
-
-
-
-
-
