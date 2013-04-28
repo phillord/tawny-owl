347907209141f153ae37d8387217532d9fb9fb70
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-04-28 11:04:03 +0100

    Significant simplication to groupify implementation.

--- src/tawny/util.clj
+++ src/tawny/util.clj
@@ -22,42 +22,19 @@
 ;; Utillity functions for improving syntax
 ;;
 (defn groupify
-  "Takes a list with keywords and values and turns all adjacent values into a single list"
-  ;; entry point
+  "Takes a list with keywords and values and turns all adjacent values into a
+  single list"
   [list]
-  (loop [reduced-frame ()
-         long-frame-left list
-         current-keyword nil
-         current-val-list nil]
-     (if-let [first-val (first long-frame-left)]
-       ;; the unreduced frame is not yet empty
-       (if (keyword? first-val)
-         ;; so we have a keyword as the first val
-         (if current-keyword 
-           ;; but we not have any vals -- exception
-           (if (not current-val-list)
-             (throw (IllegalArgumentException. "Cannot have two adjacent keywords"))
-             ;; and we have some existing keywords and values
-             (recur
-              (cons current-keyword
-                    (cons current-val-list
-                          reduced-frame))
-              (rest long-frame-left) first-val nil))
-           ;; so we have a legal new keyword, so start a new list
-           (recur
-            reduced-frame (rest long-frame-left)
-            first-val nil))
-         ;; we do not have a keyword
-         (recur reduced-frame (rest long-frame-left)
-                   current-keyword (cons first-val current-val-list)))
-       
-       ;; else we have nothing left in the frame, so terminate
-       (if (or current-keyword current-val-list)
-         (cons current-keyword
-               (cons current-val-list
-                     reduced-frame))
-         reduced-frame))))
-
+  (doall
+   (map
+    (fn [x]
+      (if (-> x (first) (keyword?))
+          (if (> (count x) 1)
+            (throw (IllegalArgumentException.
+                    "Cannot have two keywords next to each other"))
+            (first x))
+          x))
+    (partition-by keyword? list))))
 
 (defn hashify
   "Takes a list with alternating keyword values and returns a hash"
