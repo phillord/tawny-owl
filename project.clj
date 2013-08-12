9befa8a4096066e7e468b35b686906fb77a13cf7
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-08-12 14:39:24 +0100

    Bump to snapshot version due to change in OWL2Datatype.

--- project.clj
+++ project.clj
@@ -25,9 +25,9 @@
 
   :dependencies [
                  ;; owl API
-                 [net.sourceforge.owlapi/owlapi-api "3.4.5"]
-                 [net.sourceforge.owlapi/owlapi-apibinding "3.4.5"]
-                 [net.sourceforge.owlapi/owlapi-distribution "3.4.5"]
+                 [net.sourceforge.owlapi/owlapi-api "3.4.6-SNAPSHOT"]
+                 [net.sourceforge.owlapi/owlapi-apibinding "3.4.6-SNAPSHOT"]
+                 [net.sourceforge.owlapi/owlapi-distribution "3.4.6-SNAPSHOT"]
 
                  ;; clojure
                  [org.clojure/clojure "1.5.1"]
