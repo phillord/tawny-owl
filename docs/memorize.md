9955eef811fba9ba80205e1e8ab8a9511a06441e
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-10-05 15:57:35 +0100

    More documentation added.

---
+++ docs/memorize.md
@@ -0,0 +1,56 @@
+Memorizing Read Ontologies
+==========================
+
+Memorization allows tawny to cope with change in ontology dependencies, by
+tracking the identifiers that tawny has used in the past, so that it can
+recognise changes in the future. This is important for [reading](importing)
+ontologies which use hard-to-remember identifiers.
+
+
+## The problem
+
+The problem that memorize addresses was first described in an
+[article](http://www.russet.org.uk/blog/2316). Many ontologies use numeric
+identifiers; this are hard to use computationally, because they are hard for
+the developer to remember. Tawny can provide an arbitrary transformation of
+OWL annotation properties to produce a readable label. However, downstream
+ontologies may change these labels which will break any ontologies build using
+these.
+
+In a GUI environment, this causes less problems because there is a separation
+of the underlying model and what the user sees; if the label changes, and the
+identifier does not, a GUI will just show the user the update. This is less
+easy to achieve with a code base, but tawny manages this with `memorise.clj`.
+
+## Solution
+
+The memorization functionality stores the mappings between label and
+identifiers when ever a downstream OWL file is read. When a file is read for a
+second time, Tawny checks these mappings to see any of the identifiers has a
+new label. In this case, tawny effectively generates an alias to the entity
+using both the old and new label. However, use of the old label will result in
+a deprecation warning, printed to the repl. This means that downstream
+ontologies will still work, while allowing developers to migrate to new
+identifiers.
+
+## In Practice
+
+Use of memorise is relatively straight-forward. The upstream ontology needs to
+be read as described [previously](importing). To save the mappings that have
+been made, use the `memorize` function:
+
+    (tawny.memorise/memorise "./src/tawny/obi_memorise.clj")
+
+The location for the "memorise" is variable, but I use the same directory as
+the load file because it will be versioned along with other source. If you are
+using a downloaded version of the OWL source, this function only needs to be
+run every time it is updated.
+
+To remember the mappings made previously, we use the counter-part `remember`
+function.
+
+    (tawny.memorise/remember "./src/tawny/obi/obi_memorise.clj")
+
+Memorise generates a print statement for all the obsolete mappings that it
+finds, and will generate another print statement whenever the obsolete term is
+actually used.
--- src/tawny/emacs.clj
+++ src/tawny/emacs.clj
@@ -1,27 +1,32 @@
 ;; The contents of this file are subject to the LGPL License, Version 3.0.
-;; 
+;;
 ;; Copyright (C) 2013, Phillip Lord, Newcastle University
-;; 
+;;
 ;; This program is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU Lesser General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or (at your
 ;; option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful, but WITHOUT
 ;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 ;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
 ;; for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU Lesser General Public License
 ;; along with this program. If not, see http://www.gnu.org/licenses/.
 
-(ns tawny.emacs
-  [:require [tawny owl reasoner util]]
-  )
+(ns
+    ^{:doc "Support functions for tawny-mode.el"
+      :author "Phillip Lord"}
+    tawny.emacs
+  [:require [tawny owl reasoner util]])
 
+;; Executes commands and returns strings which emacs displays. Clunky but
+;; effective.
 (defn set-reasoner
+  "Sets the reasoner factory."
   [reasoner]
-  (when 
+  (when
       (tawny.reasoner/reasoner-factory reasoner)
     (format "Reasoner has been set: %s" reasoner)))
 
@@ -32,24 +37,24 @@
 Returns error message or namespace if everything is fine."
   [namespace]
   (let [ns (find-ns (symbol namespace))]
-    (cond 
+    (cond
      (nil? @tawny.reasoner/vreasoner-factory)
      "No reasoner has been set"
      (nil? ns)
      "Namespace not recognised"
      (nil? (tawny.owl/get-current-ontology ns))
      "No ontology in namespace"
-     :default 
+     :default
      ns)))
 
-(defn is-coherent 
+(defn is-coherent
   "Returns a message about the coherency of
 NAMESPACE"
   [namespace]
   (let [ns (check-namespace namespace)]
     (if (instance? String ns)
       ns
-      (if (tawny.owl/with-ontology 
+      (if (tawny.owl/with-ontology
             (tawny.owl/get-current-ontology ns)
             (tawny.reasoner/coherent?))
         "Ontology is coherent."
@@ -62,7 +67,7 @@ NAMESPACE"
   (let [ns (check-namespace namespace)]
     (if (instance? String ns)
       ns
-      (if (tawny.owl/with-ontology 
+      (if (tawny.owl/with-ontology
             (tawny.owl/get-current-ontology ns)
             (tawny.reasoner/consistent?))
         "Ontology is consistent."
@@ -70,22 +75,22 @@ NAMESPACE"
 
 
 (defn list-classes [classes]
-  (clojure.string/join 
-   "\n" 
-   (map 
+  "Given a set of classes, returns a string representation."
+  (clojure.string/join
+   "\n"
+   (map
     #(.toString %)
     classes)))
 
 (defn get-unsatisfiable [namespace]
+  "Returns a string about unsatisfiable classes."
   (let [ns (check-namespace namespace)]
     (if (instance? String ns)
       ns
-      (if-let 
-          [unsatis  
-           (seq (tawny.owl/with-ontology 
+      (if-let
+          [unsatis
+           (seq (tawny.owl/with-ontology
                   (tawny.owl/get-current-ontology ns)
                   (tawny.reasoner/unsatisfiable)))]
         (list-classes unsatis)
         "Ontology has no unsatisfiable classes."))))
-
-
