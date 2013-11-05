90e0969561b05701ea74b706218fe2b350e07d44
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-11-05 22:26:39 +0000

    Protege tracking mode. Many whitespace changes.

--- emacs/tawny-mode.el
+++ emacs/tawny-mode.el
@@ -1,17 +1,17 @@
 ;; The contents of this file are subject to the GPL License, Version 3.0.
-;; 
+;;
 ;; Copyright (C) 2013, Phillip Lord, Newcastle University
-;; 
+;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -28,7 +28,7 @@
                       (buffer-local-value 'nrepl-project-dir buffer))))
            nrepl-connection-list)
       t
-    (message 
+    (message
      "No nREPL buffer exists. Please use `nrepl-jack-in'")
     nil
     ))
@@ -36,9 +36,9 @@
 ;; select reasoner
 (defun tawny-mode-select-reasoner (reasoner)
   "Selects the current reasoner"
-  (interactive 
-   (list 
-    (completing-read 
+  (interactive
+   (list
+    (completing-read
      "Select reasoner: "
      '("hermit" "elk")
      nil t)))
@@ -47,23 +47,23 @@
         (message "You must choose a reasoner")
       (tawny-mode-nrepl-reasoner-eval-string
        (format
-        "(do (require 'tawny.emacs)(tawny.emacs/set-reasoner :%s))" 
+        "(do (require 'tawny.emacs)(tawny.emacs/set-reasoner :%s))"
         reasoner)))))
 
 (defun tawny-mode-is-coherent ()
   (interactive)
   (when (tawny-mode-check-for-nrepl-buffer)
     (tawny-mode-nrepl-reasoner-eval-string
-     (format 
+     (format
       "(do (require 'tawny.emacs)(tawny.emacs/is-coherent \"%s\"))"
       (clojure-find-ns)
       ))))
 
 (defun tawny-mode-is-consistent ()
   (interactive)
-  (when (tawny-mode-check-for-nrepl-buffer)  
+  (when (tawny-mode-check-for-nrepl-buffer)
     (tawny-mode-nrepl-reasoner-eval-string
-     (format 
+     (format
       "(do (require 'tawny.emacs)(tawny.emacs/is-consistent \"%s\"))"
       (clojure-find-ns)
       ))))
@@ -72,7 +72,7 @@
   (interactive)
   (when (tawny-mode-check-for-nrepl-buffer)
     (nrepl-send-string
-     (format 
+     (format
       "(do (require 'tawny.emacs)(tawny.emacs/get-unsatisfiable \"%s\"))"
       (clojure-find-ns))
      (tawny-mode-unsatisfiable-response-handler (current-buffer)))))
@@ -89,12 +89,12 @@
     (message msg)))
 
 (defun tawny-mode-nrepl-reasoner-eval-string (string)
-  (nrepl-send-string string 
+  (nrepl-send-string string
                      (tawny-mode-make-reasoner-response-handler (current-buffer))))
 
 
 (defun tawny-mode-make-reasoner-response-handler (buffer)
-  (nrepl-make-response-handler 
+  (nrepl-make-response-handler
    buffer
    (lambda (buffer value)
      (tawny-message "For %s: %s" buffer value))
@@ -109,14 +109,14 @@
   (get-buffer-create "*tawny-unsatisfiable*"))
 
 (defun tawny-mode-unsatisfiable-response-handler (buffer)
-  (nrepl-make-response-handler 
+  (nrepl-make-response-handler
    buffer
    (lambda (buffer value)
      (save-excursion
        (set-buffer tawny-mode-unsatisfiable-buffer)
        (erase-buffer)
        (message value)
-       (insert (format "Unsatisfiable classes for %s:\n%s" buffer 
+       (insert (format "Unsatisfiable classes for %s:\n%s" buffer
                        (tawny-de-escape value))))
      (display-buffer tawny-mode-unsatisfiable-buffer))
    (lambda (buffer value)
@@ -140,24 +140,62 @@
                        nrepl-buffer-ns
                        (nrepl-current-tooling-session))))
 
-
-
 (defun tawny-de-escape (string)
   (replace-regexp-in-string
    "\\\\n" "\n"
-   (replace-regexp-in-string 
+   (replace-regexp-in-string
     "\\\"" ""
     string)))
 
-;; nrepl-make-response-handler gives me a finer response handler. 
+
+;; Protege section
+(defvar tawny-mode-protege-entity-last nil)
+(defun tawny-mode-protege-entity ()
+  (interactive)
+  (let* ((thing
+          (substring-no-properties
+           (thing-at-point 'word)))
+         (form
+          (format "(do (require 'tawny.protege-nrepl) (tawny.protege-nrepl/display-maybe \"%s\" \"%s\"))"
+                  (clojure-find-ns) thing)))
+    (unless (equal thing tawny-mode-protege-entity-last)
+      (setq tawny-mode-protege-entity-last thing)
+      (nrepl-send-string
+       form
+       (tawny-mode-nrepl-protege-display-handler (current-buffer))
+       nrepl-buffer-ns
+       (nrepl-current-tooling-session)))))
+
+(defun tawny-mode-nrepl-protege-display-handler (buffer)
+  (nrepl-make-response-handler
+   buffer
+   (lambda (&rest value))
+   (lambda (&rest stdout))
+   (lambda (&rest stderr))
+   (lambda (&rest done))))
+
+
+(defvar tawny-mode-protege-track-timer
+  nil)
+
+(defun tawny-mode-protege-track-toggle ()
+  (interactive)
+  (setq tawny-mode-protege-track-timer
+        (if tawny-mode-protege-track-timer
+            (progn (cancel-timer tawny-mode-protege-track-timer)
+                   nil)
+          (run-with-idle-timer 0.5 t 'tawny-mode-protege-entity-idle))))
+
+
+;; nrepl-make-response-handler gives me a finer response handler.
 ;; should be able to plug this into working or equivalent, to give a nice
-;; output. 
+;; output.
 
 ;; need an "ensure-factory" thing in tawny.reasoner, so I can set the reasoner
-;; everytime. 
+;; everytime.
 (defvar tawy-mode-menu-map nil)
 
-(defvar tawny-mode-map 
+(defvar tawny-mode-map
   (let ((map (make-sparse-keymap)))
     (easy-menu-define tawny-mode-menu-map
       map "`tawny-minor-mode' menu"
@@ -172,12 +210,26 @@
          ["Hermit" (tawny-mode-select-reasoner "hermit")]
          ["Elk" (tawny-mode-select-reasoner "elk")]
          )
+        ("Protege"
+         ["Display Entity" tawny-mode-protege-entity
+          :help "Display Entity in Protege (assumes use of protege-nrepl)"]
+         ["Activate Ontology" tawny-mode-protege-ontology
+          :help "Activate Ontology "
+          ]
+         ["Track Entity" tawny-mode-protege-track-toggle
+          :help "Track the nearest entity to the cursor in Protege"
+          :active t :style radio :selected tawny-mode-protege-track-timer]
+         ["Do Not Track Entity" tawny-mode-protege-track-toggle
+          :help "Do not track the nearest entity to the cursor in Protege"
+          :active t :style radio :selected (not tawny-mode-protege-track-timer)]
+         
+         )
         ["Display Documentation" tawny-doc
          :help "Display documentation for entity at point"]
         )
       )
 
-    
+
     (define-key map (kbd "C-c s c") 'tawny-mode-is-coherent)
     (define-key map (kbd "C-c s v") 'tawny-mode-is-consistent)
     (define-key map (kbd "C-c s u") 'tawny-mode-unsatisfiable)
@@ -198,9 +250,9 @@
   (save-excursion
     (goto-char (point-min))
     (unless tawny-mode-block
-      (when 
+      (when
           (re-search-forward
-           (regexp-opt 
+           (regexp-opt
             '("defontology" "defoproperty" "defannotationproperty" "tawny.owl") 'symbols)
            (point-max) t)
         (tawny-mode 1)))))
