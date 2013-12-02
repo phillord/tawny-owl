4ab6465367cc4dbe785e65a1daeb3501f8ff9190
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-12-02 21:23:23 +0000

    Update for cider.

--- emacs/tawny-mode.el
+++ emacs/tawny-mode.el
@@ -15,7 +15,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(require 'nrepl)
+(require 'nrepl-client)
 (require 'clojure-mode)
 (require 'easymenu)
 
