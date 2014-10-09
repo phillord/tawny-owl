f12efd42eeba6a833d24acdbef6bb1e3299b4082
Phillip Lord <phillip.lord@newcastle.ac.uk>
2014-10-09 14:30:36 +0100

    Prepare for 1.2 release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uk.org.russet/tawny-owl "1.1.1"
+(defproject uk.org.russet/tawny-owl "1.2.0-SNAPSHOT"
   :description "tawny-owl enables building OWL ontologies in a programmatic environment."
   :url "http://www.russet.org.uk/blog/tawny-owl"
   :repositories [["maven" "http://repo1.maven.org/maven2"]]
