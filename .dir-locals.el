187945cd32deb79f06731eecdbfd5b7b5f70650d
Phillip Lord <phillip.lord@newcastle.ac.uk>
2016-04-25 11:46:58 +0100

    Remove .dir-locals.el to top level.
    Mostly to allow me to specify projectile-project-type
    which needs to apply to all files.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((clojure-mode
+((nil
+  (projectile-project-type . lein-test))
+ (clojure-mode
   (lentic-init . lentic-clojure-org-init)
   (cider-jack-in-auto-inject-clojure . :latest)))
