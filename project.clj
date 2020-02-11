88cdef2f86084c0033fec89a84b5b86cbbdda2b9
Phillip Lord <phillip.lord@newcastle.ac.uk>
2020-02-11 16:28:39 +0000

    Fix memorize
    memorize was not updated correctly when ontology-options ceased to
    support a default ontology.
    
    Fixes #74

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uk.org.russet/tawny-owl "2.0.1-SNAPSHOT"
+(defproject uk.org.russet/tawny-owl "2.0.2-SNAPSHOT"
   :description "tawny-owl enables building OWL ontologies in a programmatic environment."
   :url "http://www.russet.org.uk/blog/tawny-owl"
   :repositories [["maven" "https://repo1.maven.org/maven2"]]
