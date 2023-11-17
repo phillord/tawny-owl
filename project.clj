57f3913e83abbc460126880a5db2b2dbcbe2f555
Phillip Lord <phillip.lord@newcastle.ac.uk>
2023-11-17 16:43:17 +0000

    Update all libraries
    Thanks to mosfet80 for initial suggestion

--- project.clj
+++ project.clj
@@ -28,11 +28,11 @@
 
   :dependencies [
                  ;; owl API
-                 [net.sourceforge.owlapi/owlapi-distribution "4.5.19"]
+                 [net.sourceforge.owlapi/owlapi-distribution "4.5.26"]
 
                  ;; clojure
-                 [org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.logic "0.8.11"]
+                 [org.clojure/clojure "1.11.1"]
+                 [org.clojure/core.logic "1.0.1"]
 
                  ;; reasoners
                  [org.semanticweb.elk/elk-owlapi "0.4.3"]
@@ -47,7 +47,7 @@
                  ;; Shut up ELK
                  [org.apache.logging.log4j/log4j "2.21.1" :extension "pom"]
                  ;; Shut up OWL API
-                 [org.slf4j/slf4j-nop "1.7.10"]
+                 [org.slf4j/slf4j-nop "2.0.9"]
 
                  ;; identitas
                  [uk.org.russet/identitas-j "0.0.1"]]
@@ -56,7 +56,10 @@
   ;; dependency adds dev-resources to the path which I need for testing.
   :profiles
   {
-    :1.9
+   :1.10
+   [:base
+    {:dependencies [[org.clojure/clojure "1.10.0"]]}]
+   :1.9
    [:base
     {:dependencies [[org.clojure/clojure "1.9.0"]]}]
    })
